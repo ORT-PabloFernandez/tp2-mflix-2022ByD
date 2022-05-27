2d256ba6b8f2e541ee31a906890a33cad5be25fa
soph2022 <102108525+soph2022@users.noreply.github.com>
2022-05-26 21:55:07 -0300

    add

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/');
+const controller = require('../controllers/users');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
