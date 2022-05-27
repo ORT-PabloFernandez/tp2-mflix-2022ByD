b742ad0606a740c7c99d7c3e90c53b7d292d8ac6
MGAmarillo <mateo.amarillo50@gmail.com>
2022-05-26 22:38:40 -0300

    Parcial TP2 - Mateo Amarillo - BETP2B

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,60 @@
 const movies = require('../data/movies');
+const { all } = require('../routes/movies');
 
-async function getAllMovies(pageSize, page){    
+async function getAllMovies(pageSize, page) {
     return movies.getAllMovies(pageSize, page);
 }
 
-module.exports = {getAllMovies};
+async function getMovie(id) {
+    return movies.getOneMovie(id);
+}
+
+async function getMovieByAward(pageSize, page) {
+    let allMovies = await movies.getAllMovies(pageSize, page);
+    let awardMovies = allMovies.filter(movie => movie.awards.wins >= 1);
+    let mapMovies = awardMovies.map(movie => ({ title: movie.title, poster: movie.poster, plot: movie.plot }));
+    return mapMovies;
+
+}
+
+async function getMovieByLanguage(language, pageSize, page,) {
+    let allMovies = await movies.getAllMovies(pageSize, page);
+
+    let moviesWithoutUndefined = allMovies.filter(movie => movie.languages != undefined);
+
+    let moviesByLanguage = [];
+
+    moviesWithoutUndefined.forEach(movie => {
+    let conditional = false;
+    
+    movie.languages.forEach(movieLanguage => {
+
+    if(movieLanguage == language){
+        conditional = true;
+
+
+
+        if(conditional == true){
+            moviesByLanguage.push(movie);
+        }
+    }
+    })
+
+    });
+
+    return moviesByLanguage;
+
+}
+
+async function getMovieByTomatoes(pageSize, page) {
+    let allMovies = await movies.getAllMovies(pageSize, page);
+    let moviesByTomatoes = allMovies.sort((movieA, movieB) => (movieA.tomatoes.fresh > movieB.tomatoes.fresh) ? -11:1);
+    return moviesByTomatoes;
+
+}
+
+
+
+
+module.exports = { getAllMovies, getMovie, getMovieByAward, getMovieByLanguage, getMovieByTomatoes };
+
--- routes/movies.js
+++ routes/movies.js
@@ -2,11 +2,34 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/movies');
 
-router.get('/', async (req, res) => {    
-    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize): 0;
-    const page = req.query.page ? parseInt(req.query.page): 0;
-    
+router.get('/', async (req, res) => {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+
     res.json(await controller.getAllMovies(pageSize, page));
 });
+
+router.get('/:id', async (req, res) => {
+    res.json(await controller.getMovie(req.params.id));
+});
+
+router.get('/awards/movieByAwards', async (req, res) => {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    res.json(await controller.getMovieByAward(pageSize, page));
+});
+
+router.get('/languages/:language', async (req, res) => {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    res.json(await controller.getMovieByLanguage(req.params.language, pageSize, page));
+});
+
+router.get('/tomatoes/fresh', async (req, res) => {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    res.json(await controller.getMovieByTomatoes(pageSize, page));
+});
+
 
 module.exports = router;
