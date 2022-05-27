077342787959e9931c647d05fe6a129bef6c5792
Camila Belen Capua <camilabelencapua@gmail.com>
2022-05-26 22:20:30 -0300

    get por orden

--- routes/movies.js
+++ routes/movies.js
@@ -40,8 +40,11 @@ router.get('/', async (req, res) => {
 router.get('/ordenTomatoes', async (req, res) => {    
     const pageSize = req.query.pageSize ? parseInt(req.query.pageSize): 0;
     const page = req.query.page ? parseInt(req.query.page): 0;
-    
-    res.json(await controller.getOrdenTomatoes(pageSize, page));
+    try{
+        res.json(await controller.getOrdenTomatoes(pageSize, page));
+    }catch(err){
+        res.sendStatus(400).json(err)
+    }
 });
 
 module.exports = router;
