137ba36dc236b1ba1bebca7d66c6d499a1e915ac
Cata Buljevich <catalinabuljevich@gmail.com>
2022-05-26 21:20:55 -0300

    guardado

--- data/movies.js
+++ data/movies.js
@@ -1,16 +1,62 @@
-const conn = require('./conn');
-const DATABASE = 'sample_mflix';
-const MOVIES = 'movies';
+const conn = require("./conn");
+const DATABASE = "sample_mflix";
+const MOVIES = "movies";
+const objectId = require("mongodb").ObjectId;
 
-
-async function getAllMovies(pageSize, page){
-    const connectiondb = await conn.getConnection();
-    const movies = await connectiondb
-                        .db(DATABASE)
-                        .collection(MOVIES)
-                        .find({}).limit(pageSize).skip(pageSize * page)
-                        .toArray();    
-    return movies;
+async function getAllMovies(pageSize, page) {
+  const connectiondb = await conn.getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({})
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return movies;
 }
 
-module.exports = {getAllMovies};
+async function getMoviebyId(id) {
+  const connectiondb = await conn.getConnection();
+  const movie = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ _id: new objectId(id) })
+    .toArray();
+  return movie;
+}
+
+async function getAwardWinningMovies(amount) {
+  const connectiondb = await conn.getConnection();
+  const aWMovies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ "awards.wins": { $gte: parseInt(amount) } })
+    .map((movie) => ({
+      plot: movie.plot,
+      poster: movie.poster,
+      title: movie.title,
+    }))
+    .toArray();
+
+  return aWMovies;
+}
+
+async function getMoviesByLanguage(lang, pageSize, page) {
+  const connectiondb = await conn.getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ languages: [lang] })
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  console.log(lang);
+  return movies;
+}
+
+module.exports = {
+  getAllMovies,
+  getMoviebyId,
+  getAwardWinningMovies,
+  getMoviesByLanguage,
+};
--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/movies');
+const controller = require("../controllers/movies");
+const data = require(".././data/movies");
 
-router.get('/', async (req, res) => {    
-    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize): 0;
-    const page = req.query.page ? parseInt(req.query.page): 0;
-    
-    res.json(await controller.getAllMovies(pageSize, page));
+router.get("/", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+
+  res.json(await controller.getAllMovies(pageSize, page));
+});
+
+router.get("/:id", async (req, res) => {
+  const movie = await data.getMoviebyId(req.params.id);
+  res.json(movie);
+});
+
+router.get("/wins/:wins", async (req, res) => {
+  const movies = await data.getAwardWinningMovies(req.params.wins);
+  res.json(movies);
+});
+
+router.get("/language/:language", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  const movies = await data.getMoviesByLanguage(
+    req.params.language,
+    pageSize,
+    page
+  );
+  res.json(movies);
 });
 
 module.exports = router;
