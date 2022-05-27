d245c09dad56df969c1759b52f5349f1b8e4f34a
soph2022 <102108525+soph2022@users.noreply.github.com>
2022-05-26 21:31:24 -0300

    added user controller

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const controller = require('../controllers/users.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log('checkl');
-  res.send('respond with a resource');
+  res.json(await controller.getAllUsers());
+});
+
+router.get('/:user_id', function(req, res, next) {
+  console.log('checkl');
+  const user_id = req.params.user_id;
+  res.json(await controller.getUserById(user_id));
 });
 
 module.exports = router;
