5fdaae17b8968a94743d4178a646dbd0b8f45bf4
svalino95 <55114752+svalino95@users.noreply.github.com>
2022-05-26 22:24:40 -0300

    Santiago Valiño Silva

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,36 @@
 const movies = require('../data/movies');
 
+
 async function getAllMovies(pageSize, page){    
     return movies.getAllMovies(pageSize, page);
 }
 
-module.exports = {getAllMovies};
+// 1 - Necesitamos un endpoint que nos devuelva una película (movie) particular por _id
+
+async function getMovie (id) {
+
+    return movies.getMovie(id);
+}
+
+// 2 - 
+async function getMovieConPremios () {
+
+    return movies.getMovieConPremios();
+}
+
+// 3 -
+
+
+async function getMovieLenguage(language, pageSize, page) {
+    return movies.getMovieLenguage(language, pageSize, page);
+  }
+
+
+  // 4- 
+
+  async function getMoviesPorTomatoes(){
+      return movies.getMoviesPorTomatoes();
+  }
+
+  
+module.exports = {getAllMovies, getMovie, getMovieConPremios,getMovieLenguage,getMoviesPorTomatoes};
